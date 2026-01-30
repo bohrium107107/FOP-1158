68be247fc998e0b205d293f58e4dd52056518b7a
bohrium107107 <akanksha.m.dange@gmail.com>
2026-01-30 06:48:33 +0000

    sum of two arrays

---
+++ array2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main() {
+    int a[3];
+    int b[3];
+    int c[3];
+    printf("Enter three integers for array a:\n");
+    for (int i=0; i<3; i++) {
+        scanf("%d", &a[i]); }
+    printf("Enter three integers for array b:\n");
+    for (int i=0; i<3; i++) {
+        scanf("%d", &b[i]); }
+    for (int i=0; i<3; i++) {
+        c[i] = a[i] + b[i]; }
+    printf("The resulting array c (a[i] + b[i]) is:\n");
+    for (int i=0; i<3; i++) {
+        printf("%d ", c[i]); }
+    }
