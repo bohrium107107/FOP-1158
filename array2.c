37272f9c69c56e51bafc767f622b16f91c334fa7
bohrium107107 <akanksha.m.dange@gmail.com>
2026-01-30 07:07:24 +0000

    Multiplication of two arrys

--- array2.c
+++ array2.c
@@ -13,7 +13,7 @@ void main() {
     
         //addition of two arrays
     for (int i=0; i<3; i++) {
-        c[i] = a[i] + b[i]; }
+        c[i] = a[i] * b[i]; }
 
     printf("The resulting array c (a[i] + b[i]) is:\n");
     for (int i=0; i<3; i++) { // display c
