0836fda8e7d2dc8f7159cbda9aac9b4ba33181bc
bohrium107107 <akanksha.m.dange@gmail.com>
2026-02-13 11:37:07 +0530

    Enhance program to check for prime numbers
    Added functionality to check if the number is prime and print prime factors.

--- Assignment/Assignment 11.c
+++ Assignment/Assignment 11.c
@@ -1,4 +1,4 @@
-//Comput square, square root, cube and cube root of a number, factorial
+//Comput square, square root, cube and cube root of a number, factorial, check if its prime number, prime factors
 #include <stdio.h>
 #include <math.h>
 int main() {
@@ -13,10 +13,19 @@ int main() {
     printf("Square root: %.2f\n",sqrt(n));
     printf("Cube: %lld\n",(long long)n*n*n);
     printf("Cube root: %.2f\n",cbrt(n));
-    long long fact = 1; //only valid up to 20!
+    
+    long long fact = 1; 
     for (int i=1; i<=n; i++) {
         fact *= i;
     }
     printf("Factorial: %lld",fact);
+
+    for (int i = 2; i<(n/2) ; i++ ){
+        if (n%i == 0) {
+            printf("Not prime")
+                }
+        else {
+            printf("Is prime")}
+    
     return 0;
 }
