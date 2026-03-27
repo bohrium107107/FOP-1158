dcb398f17d072e7f3be2eb7a02b37780abf9a98f
bohrium107107 <akanksha.m.dange@gmail.com>
2026-03-27 06:15:15 +0000

    factorial

--- Assignment/Assignment_6.c
+++ Assignment/Assignment_6.c
@@ -10,11 +10,34 @@ int factorial(int n) {
 }
 
 int main() {
-  printf("Enter number ");
+  printf("Enter positive number ");
   scanf("%d", &m);
-  printf("Factorial is %d", factorial(m));
+  if (m < 0) {
+        printf("Error! Factorial of a negative number doesn't exist.\n");
+    } else {
+  printf("Factorial is %d", factorial(m));}
   return 0;
 }
 
+/*int main() {
+    int n, i;
+    long long factorial = 1;
 
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    
+    if (n < 0) {
+        printf("Error! Factorial of a negative number doesn't exist.\n");
+    } else {
+        
+        for (i = n; i >= 1; i--) {
+            factorial *= i; 
+        }
+        printf("Factorial of %d = %lld\n", n, factorial);
+    }
+
+    return 0;
+}
+*/
 
