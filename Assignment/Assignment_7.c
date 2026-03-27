aa734fd92a86c4288ea75db210ae40d086207219
bohrium107107 <akanksha.m.dange@gmail.com>
2026-03-27 06:32:46 +0000

    structure of student

--- Assignment/Assignment_7.c
+++ Assignment/Assignment_7.c
@@ -3,24 +3,23 @@
 struct student {
     char name[50];
     int roll_number;
-    char class[10];
+    char clas[10];
     float marks1;
     float marks2;
     float average;
 };
 
 int main() {
-    struct student s[1];
-    for (int i = 0; i < 1; i++) {
-        printf("Enter student name: ");
-        scanf("%s", s[i].name);
-
+    struct student s[2];
+    for (int i = 0; i < 2; i++) {
+        printf("Enter student %d name: ", i + 1);
+        scanf("%s", s[i].name); 
         printf("Enter roll number: ");
         scanf("%d", &s[i].roll_number);
     
     
     printf("Enter class: ");
-    scanf("%s", s[i].class);
+    scanf("%s", s[i].clas);
     
     printf("Enter marks in subject 1: ");
     scanf("%f", &s[i].marks1);
@@ -33,7 +32,7 @@ int main() {
     printf("\n--- Student Information ---\n");
     printf("Name: %s\n", s[i].name);
     printf("Roll Number: %d\n", s[i].roll_number);
-    printf("Class: %s\n", s[i].class);
+    printf("Class: %s\n", s[i].clas);
     printf("Subject 1 Marks: %.2f\n", s[i].marks1);
     printf("Subject 2 Marks: %.2f\n", s[i].marks2);
     printf("Average Marks: %.2f/100\n", s[i].average);
