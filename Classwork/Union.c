dfd3567f052944496ead63184ebd583132d6eec8
Akanksha <akanksha.m.dange@gmail.com>
2026-03-30 14:07:41 +0530

    union

---
+++ Classwork/Union.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    struct a{
+        int i;
+        char ch[2];
+    };
+    struct a key;
+    key.i = 512;
+    strcpy(key.ch,"d");
+    printf("key.i= %d\n", key.i);
+    printf("key.ch[0] = %s\n",key.ch);
+    printf("key.ch[1] = %s\n",key.ch);
+    key.i= 42;
+    printf("key.i = %d\n", key.i);
+    printf("key.ch[0] = %s\n",key.ch);
+    return 0;
+
+}
