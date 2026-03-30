16b3369000aa1e4c8ae5e5a1bac94b23bb60d2ae
Akanksha <akanksha.m.dange@gmail.com>
2026-03-30 14:09:38 +0530

    union

--- Classwork/Union.c
+++ Classwork/Union.c
@@ -14,6 +14,25 @@ int main() {
     key.i= 42;
     printf("key.i = %d\n", key.i);
     printf("key.ch[0] = %s\n",key.ch);
+    printf("------------------------------------------");
+
+
+    union b{
+        int i;
+        char ch[2];
+    };
+    union b key;
+    key.i = 512;
+    strcpy(key.ch,"d");
+    printf("key.i= %d\n", key.i);
+    printf("key.ch[0] = %s\n",key.ch);
+    printf("key.ch[1] = %s\n",key.ch);
+    key.i= 42;
+    printf("key.i = %d\n", key.i);
+    printf("key.ch[0] = %s\n",key.ch);
+    
+
+
     return 0;
 
 }
