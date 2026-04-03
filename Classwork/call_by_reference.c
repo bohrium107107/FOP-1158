5b8a0d19537c9cc6fe7b0f8969a44e55edab3685
bohrium107107 <akanksha.m.dange@gmail.com>
2026-04-03 06:37:36 +0000

    call_by_reference

--- Classwork/call_by_reference.c
+++ Classwork/call_by_reference.c
@@ -10,14 +10,24 @@ void swapbyref(int *a, int *b){
     int temp = *a;
     *a = *b;
     *b = temp;
-    printf("a by ref =%d, b by ref =%d ", *a,*b);
+    printf("a by ref = %d, b by ref = %d ", *a,*b);
 }
 
 
 int main() {
-    int a = 2;
-    int b = 3;
-    swap(2,3);
-    swapbyref(&a,&b);
+    int a ;
+    int b,c ;
+    printf("Enter two numbers a and b: ");
+    scanf("%d %d", &a, &b);
+    printf("Enter choice: 1 for call by value, 2 for call by reference: ");
+    scanf("%d", &c);
+    switch (c){
+    case 1:
+        swap(a,b);
+        break;
+    case 2:        
+        swapbyref(&a,&b);
+        break;
+    }
     return 0;
 }
